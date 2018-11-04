06fc7ae456272c4cd3f38dec6caa6293fec4641e
Adam Urbina <adamurbina@gmail.com>
2018-11-04 17:46:39 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -8,7 +8,7 @@ module ArtistsHelper
     if song.artist == artist
       hidden_field_tag "song[artist_id]", song.artist_id
     else
-      select_tag "song[artist_id]", options_from_collection_for_select[Artist.all, :id, :name]
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     end
   end
 
